2ca538e96d33fb7854e08252fd32c83cc8021b5d
Phan Huy Hoang <hoangph271@gmail.com>
2022-06-29 06:37:06 +0700

    [Add] .flv to .mp3

--- yt-list-download/index.js
+++ yt-list-download/index.js
@@ -1,10 +1,13 @@
-const fs = require('fs')
 const path = require('path')
 const ytdl = require('ytdl-core')
 const ytpl = require('ytpl')
+const ffmpeg = require('fluent-ffmpeg')
 const sanitizeFilename = require('sanitize-filename')
 
-const { VIDEO_DIR = 'videos' } = process.env
+const {
+  VIDEO_DIR = 'videos',
+  FFMPEG_PATH = '/opt/homebrew/bin/ffmpeg'
+} = process.env
 
 const LIST_URL = 'https://www.youtube.com/playlist?list=PLpISLnShJQ2a0n2i_h6DPtOWn_v_KhL-S'
 
@@ -14,17 +17,23 @@ async function main() {
   for (let i = 0; i < items.length; i++) {
     const videoUrl = items[i].url
     const { title } = (await ytdl.getBasicInfo(videoUrl, {})).videoDetails
-    const videoPath = path.join(VIDEO_DIR, `${sanitizeFilename(title)}.flv`)
+    const videoPath = path.join(VIDEO_DIR, `${sanitizeFilename(title)}.mp3`)
 
     await new Promise(resolve => {
-      const ws = fs.createWriteStream(videoPath)
-
-      ws.once('finish', resolve)
-
-      ytdl(videoUrl, {
+      const source = ytdl(videoUrl, {
         filter: 'audioonly',
         format: 'flv',
-      }).pipe(ws)
+      })
+
+      const process = new ffmpeg({ source })
+
+      process.setFfmpegPath(FFMPEG_PATH)
+      process.withAudioCodec('libmp3lame')
+        .toFormat('mp3')
+        .output(require('fs').createWriteStream(videoPath))
+        .run()
+
+      process.on('end', resolve)
     })
 
     console.info(`Downloaded [${i}/${items.length}]: ${videoPath}`)
