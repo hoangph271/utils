aa543cb46194c2b516f9d999bdd368355a075057
Phan Huy Hoang <hoangph271@gmail.com>
2022-11-06 22:41:26 +0700

    [Add] inputs.js

---
+++ yt-list-download/inputs.js
@@ -0,0 +1,11 @@
+// `directory` - to the FS for offline storage
+// `url` - to the requested playlist
+// `onlyAudio` - download as .mp3 files
+
+module.exports = [
+  {
+    directory: '',
+    url: '',
+    onlyAudio: true,
+  },
+]
