febc9e74f5c02ad822905f3e28628aeb0cdf381f
Phan Huy Hoang <hoangph271@gmail.com>
2023-04-15 01:14:46 +0700

    [Add] useMusics/useThemes

--- yt-list-download/inputs.js
+++ yt-list-download/inputs.js
@@ -4,8 +4,14 @@
 
 module.exports = [
   {
-    directory: '/Users/garand/useSynced/useMusics',
+    directory: '~/useSynced/useMusics/useLords',
     url: 'https://www.youtube.com/playlist?list=PLpISLnShJQ2a0n2i_h6DPtOWn_v_KhL-S',
+    onlyAudio: true,
+    needsCleanup: false,
+  },
+  {
+    directory: '~/useSynced/useMusics/useThemes',
+    url: 'https://www.youtube.com/playlist?list=PLpISLnShJQ2ZjgNYYE_-uxToaHGrRGRHY',
     onlyAudio: true,
     needsCleanup: false,
   },
